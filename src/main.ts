4eaee6abc8874c44fc2a6349cefb7b543019c368
WernerReyes <werner.reyes@tecsup.edu.pe>
2025-11-11 17:38:15 -0500

    feat: Añadir comentario en el código para investigar un problema en la sección de creación de empresa

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 import { AppComponent } from './app.component';
 
+// TODOS:
+ //* Check why when i create our company section for any reasion the phrase does not appear
+
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));

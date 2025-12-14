6f35f7d5818cdc22e962f44b2aede5be7104a66a
Werner Reyes <reyespilcowerner@gmail.com>
2025-12-14 01:48:53 -0500

    feat: Optimizar la lógica de autenticación y manejo de inicialización en los guards y el servicio de autenticación

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,15 +1,21 @@
+import { authInterceptor } from '@/auth/interceptors/auth.interceptor';
+import { AuthService } from '@/auth/services/auth.service';
+import { handlerErrorInterceptor } from '@/shared/interceptors/handler-error.interceptor';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
+import { firstValueFrom } from 'rxjs';
 import { appRoutes } from './app.routes';
-import { authInterceptor } from '@/auth/interceptors/auth.interceptor';
-import { handlerErrorInterceptor } from '@/shared/interceptors/handler-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
+        provideAppInitializer(() => {
+            const authService = inject(AuthService);
+            return firstValueFrom(authService.me());
+        }),
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
         provideHttpClient(withFetch(), withInterceptors([authInterceptor, handlerErrorInterceptor])),
         provideAnimationsAsync(),
--- src/app/auth/guards/not-authenticated-guard.ts
+++ src/app/auth/guards/not-authenticated-guard.ts
@@ -1,15 +1,20 @@
 import { inject } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { CanActivateFn, Router } from '@angular/router';
+import { filter, firstValueFrom } from 'rxjs';
 import { AuthService } from '../services/auth.service';
-import { firstValueFrom } from 'rxjs';
 
 export const notAuthenticatedGuard: CanActivateFn = async (route, state) => {
     const authService = inject(AuthService);
     const router = inject(Router);
 
-    const isAuthenticated = await firstValueFrom(authService.me());
-   
-    if (isAuthenticated) {
+    await firstValueFrom(
+        toObservable(authService.initialized).pipe(
+            filter((init) => init) // solo pasa cuando initialized = true
+        )
+    );
+
+    if (authService.isAuthenticated()) {
         router.navigateByUrl('/dashboard');
         return false;
     }
